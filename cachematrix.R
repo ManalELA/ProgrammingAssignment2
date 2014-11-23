6481c5bbd0c688dd402d60e2265dc4a7dc8f2099
Manal <manal.elakrouchi@gmail.com>
2014-11-23 18:22:35 +0000

    Solution of the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## In this function, we will test first if the matrix is square or not,
+## and then create the special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  d<-dim(x)
+  if(d[1]==d[2]){
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  }
+  else{
+    message("This matrix is not square!")
+  }
 }
 
 
-## Write a short comment describing this function
+## this function will return the inverse of the matrix based on the result of the first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
